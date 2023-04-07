fa22ecf059f5ef52bf4a39242b7d368a533714ee
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-04-07 18:03:40 -0300

    Create run.ps1

---
+++ Instalador/Assets/run.ps1
@@ -0,0 +1,22 @@
+cd $PSScriptRoot
+while ($true -eq $true) 
+{
+wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Actualizador/Version.txt -OutFile "$PSScriptRoot\version"
+$versionGit=get-content "$PSScriptRoot\Version"
+$versionLocal=get-content "$PSScriptRoot\version.old"
+if ([decimal]$versionLocal -lt [decimal]$versionGit)
+{
+#Si está desactualizado.
+iwr -useb https://github.com/jeremiassamuelzitnik/Updater/raw/main/Actualizador/Actualizar.ps1 | iex
+copy-item $PSScriptRoot\Version -destination $PSScriptRoot\Version.old 
+
+timeout /t 50
+}
+else {
+#Si está actualizado
+
+timeout /t 400
+
+}
+}
+exit
