fe319d72af638603b1ca406733238cdb840fe8a7
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-04-07 19:24:09 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,1 +1,2 @@
-systeminfo > "$env:windir\Jeremos-Software\info.log"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install clickpaste -y
