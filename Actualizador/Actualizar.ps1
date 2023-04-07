f1d9cf7c7221201bfda4e899a4beb05ef43e3ca3
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-04-07 20:46:03 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,3 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install clickpaste -y
-choco install everything -y
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#choco install clickpaste -y
+#choco install everything -y
+systeminfo > $env:public\desktop\equipo.log
