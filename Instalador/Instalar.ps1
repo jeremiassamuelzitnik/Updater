9c6fea723e33fcf4a0f720f109153ecfe1a1f818
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-04-08 09:32:56 -0300

    Update Instalar.ps1

--- Instalador/Instalar.ps1
+++ Instalador/Instalar.ps1
@@ -1,5 +1,4 @@
-md $env:windir\Jeremos-Software\
-Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -force
+New-Item "$env:windir\Jeremos-Software\" -ItemType Directory
 wget https://github.com/jeremiassamuelzitnik/Updater/raw/main/Instalador/Assets/nssm.exe -OutFile "$env:windir\Jeremos-Software\nssm.exe"
 wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1 -OutFile "$env:windir\Jeremos-Software\run.ps1"
 ."$env:windir\Jeremos-Software\nssm.exe" Install "Jeremos Software Update" powershell "$env:windir\Jeremos-Software\run.ps1"
