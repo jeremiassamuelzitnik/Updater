9a7a7170de0804a71057717c9f6c4f491fd515a4
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-04-10 23:18:15 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,7 +1,6 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 #choco install clickpaste -y
 #choco install everything -y
-#systeminfo > "$env:public\desktop\$env:computername.log"
+systeminfo > "$env:public\desktop\$env:computername.log"
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
-wget "https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1" -outfile "$PSScriptRoot\run.ps1"
-pause
+
