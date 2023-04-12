23529c8510a8055c9a7e0e8d05fd2d0d8c7365af
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-04-11 23:53:14 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -2,6 +2,6 @@
 #choco install clickpaste -y
 #choco install everything -y
 systeminfo > "$env:public\desktop\$env:computername.log"
-powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
+#powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
 
