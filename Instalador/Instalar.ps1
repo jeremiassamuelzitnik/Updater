80afac22cdc8b36d492e74bc69cb28e29a2edb5e
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-04-15 22:59:17 -0300

    Update Instalar.ps1

--- Instalador/Instalar.ps1
+++ Instalador/Instalar.ps1
@@ -1,5 +1,6 @@
 New-Item "$env:windir\Jeremos-Software\" -ItemType Directory
-wget https://github.com/jeremiassamuelzitnik/Updater/raw/main/Instalador/Assets/nssm.exe -OutFile "$env:windir\Jeremos-Software\nssm.exe"
-wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1 -OutFile "$env:windir\Jeremos-Software\run.ps1"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/jeremiassamuelzitnik/Updater/raw/main/Instalador/Assets/nssm.exe","$env:windir\Jeremos-Software\nssm.exe")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1", "$env:windir\Jeremos-Software\run.ps1")
 ."$env:windir\Jeremos-Software\nssm.exe" Install "Jeremos Software Update" powershell "$env:windir\Jeremos-Software\run.ps1"
 Restart-Service "Jeremos Software Update"
