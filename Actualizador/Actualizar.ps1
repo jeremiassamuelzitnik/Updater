cbde564fe3466deeed445881d41e88b48221d589
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-01 16:44:27 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -2,7 +2,7 @@
 #choco install clickpaste -y
 #choco install everything -y
 systeminfo >> "$env:temp\$env:computername.log"
-(New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php','$env:temp\' + $env:computername + '.log')
+(New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php', $env:temp + '\'+ $env:computername + '.log')
 #powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
 
