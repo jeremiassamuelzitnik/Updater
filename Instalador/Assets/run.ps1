ee558c0e756e7e7114bc8970a2433acda2860134
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-01 21:46:55 -0300

    Update run.ps1

--- Instalador/Assets/run.ps1
+++ Instalador/Assets/run.ps1
@@ -13,6 +13,49 @@ Set-Location $PSScriptRoot
 
 #Verificamos si existen actualizaciones con respecto a nuestro proyecto de GitHub.
 while ($true -eq $true) 
+# Function to check internet connectivity
+function Test-InternetConnection {
+    $url = "http://www.google.com"
+    try {
+        $request = [System.Net.WebRequest]::Create($url)
+        $request.Timeout = 5000
+        $response = $request.GetResponse()
+        $response.Close()
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+# Check internet connectivity
+$internetConnected = $false
+
+while (-not $internetConnected) {
+    if (Test-InternetConnection) {
+        $internetConnected = $true
+        Write-Host "Internet connection is available. Proceeding with the script."
+
+        # Your code goes here
+        # For example, you can add your commands or functions here.
+
+    }
+    else {
+        Write-Host "No internet connection available. Waiting for 30 seconds to recheck..."
+        Start-Sleep -Seconds 30
+    }
+}
+
+# Check internet connectivity
+if (Test-InternetConnection) {
+    Write-Host "Internet connection is available. Proceeding with the script."
+    # Your code goes here
+}
+else {
+    Write-Host "No internet connection available. Please check your internet connection and try again."
+    # Additional actions or error handling if needed
+}
+
 {
 $versionGitJeremosSoftware=[decimal](wget "$urlVerJeremosSoftware" -ErrorAction SilentlyContinue).Content
 $versionLocalJeremosSoftware=[decimal](Get-Content "$PSScriptRoot\version" -ErrorAction SilentlyContinue)
@@ -23,10 +66,10 @@ if ($versionLocalJeremosSoftware -lt $versionGitJeremosSoftware)
 #Si desactualizado.
 
 #Ejecutamos el script.
-iwr -useb "$urlEjecutableJeremosSoftware" | iex
+Invoke-WebRequest -useb "$urlEjecutableJeremosSoftware" | iex
 
 #Actualizamos la version del archivo local.
-wget "$urlVerJeremosSoftware" -OutFile "$PSScriptRoot\version"
+Invoke-WebRequest "$urlVerJeremosSoftware" -OutFile "$PSScriptRoot\version"
 
 }
 
