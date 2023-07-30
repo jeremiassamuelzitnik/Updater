ff94cb0d766c340675a19ab112f25aa134382f32
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-30 14:20:41 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -34,8 +34,8 @@ $trigger = New-ScheduledTaskTrigger -AtStartup
 $task = Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -Description $taskDescription -User "NT AUTHORITY\SYSTEM"
 
 # Ejecuta la tarea ahora
-start-process "powershell" -ArgumentList "Start-Sleep -Seconds 10; Start-ScheduledTask -TaskName $taskName"
 '2.32' | Out-File "$env:winver\version"
+start-process "powershell" -ArgumentList "Start-ScheduledTask -TaskName $taskName"
 $WebClient.DownloadFile("https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1", "$env:windir\Jeremos-Software\run.ps1")
 exit
 }
