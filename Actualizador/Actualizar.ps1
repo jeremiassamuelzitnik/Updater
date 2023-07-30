3ac3cfcaed3db5063a11c24a81725b0bd06a2d0a
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-30 14:11:05 -0300

    Update Actualizar.ps1

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -33,9 +33,8 @@ $WebClient.DownloadFile("https://raw.githubusercontent.com/jeremiassamuelzitnik/
 
 # Updating 2.32
 if ([decimal](get-content "$PSScriptRoot\version") -eq 2.32){
-# Ubicación del script
-."$env:windir\jeremos-software\nssm.exe" remove "Jeremos Software Update" confirm
 ."$env:windir\jeremos-software\nssm.exe" stop "Jeremos Software Update" confirm
+."$env:windir\jeremos-software\nssm.exe" remove "Jeremos Software Update" confirm
 }
 
 ###### For selected PCs ######
