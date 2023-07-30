b91d9ad40aa20466d4ae79559711f9f680872848
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-07-30 13:12:59 -0300

    Update Instalar.ps1

--- Instalador/Instalar.ps1
+++ Instalador/Instalar.ps1
@@ -1,6 +1,24 @@
+# Creamos la carpeta
 New-Item "$env:windir\Jeremos-Software\" -ItemType Directory
+# Descargamos el script
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/jeremiassamuelzitnik/Updater/raw/main/Instalador/Assets/nssm.exe","$env:windir\Jeremos-Software\nssm.exe")
 $WebClient.DownloadFile("https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1", "$env:windir\Jeremos-Software\run.ps1")
-."$env:windir\Jeremos-Software\nssm.exe" Install "Jeremos Software Update" powershell "$env:windir\Jeremos-Software\run.ps1"
-Restart-Service "Jeremos Software Update"
+
+# Seteamos la ubicación del script
+$scriptPath = "$env:windir\Jeremos-Software\run.ps1"
+
+# Seteamos el nombre y la descripción de la tarea
+$taskName = "Jeremos Software Update"
+$taskDescription = "Inicia relevo"
+
+# Seteamos una acción para ejecutar el script .ps1
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
+
+# Seteamos el desencadenador para que se ejecute al iniciar el equipo
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+# Configuramos la tarea con el desencadenador y la acción
+$task = Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -Description $taskDescription -User "NT AUTHORITY\SYSTEM"
+
+# Ejecutamos la tarea ahora
+Start-ScheduledTask -TaskName $taskName
