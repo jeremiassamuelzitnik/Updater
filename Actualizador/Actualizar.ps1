58a46d85b01ceb38714d5fab9da2f3d154b20ab0
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2024-01-02 21:03:16 -0300

    .

--- Actualizador/Actualizar.ps1
+++ Actualizador/Actualizar.ps1
@@ -1,15 +1,19 @@
-#Settings
-$WebClient = New-Object System.Net.WebClient
-$UploadPHP = 'https://www.mistrelci.com.ar/Script/upload.php'
-$GitRunPS1 = 'https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1'
-$GitTimeout = 'https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Actualizador/timeout.txt'
-$defaultLog = "$env:windir\Jeremos-Software\Logs\$env:computername.log"
-if (-not (Test-Path "$env:windir\Jeremos-Software\Logs")) {mkdir "$env:windir\Jeremos-Software\Logs"}
-
 #Getting Computer info
 $computerInfo = Get-ComputerInfo | Select-Object *
 $biosSN = Get-WmiObject -Class Win32_BIOS | Select-Object SerialNumber
 $formattedDate = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+
+#Parameters
+$WebClient = New-Object System.Net.WebClient
+$UploadPHP = 'https://www.mistrelci.com.ar/Script/upload.php'
+$GitRunPS1 = 'https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Instalador/Assets/run.ps1'
+$GitTimeout = 'https://raw.githubusercontent.com/jeremiassamuelzitnik/Updater/main/Actualizador/timeout.txt'
+$DefaultLog = $env:windir + '\Jeremos-Software\Logs\' + $env:computername + '-' + $biosSN.SerialNumber + '-' + $computerInfo.OsSerialNumber + '.log'
+
+#Creating Logs folder if not exits
+if (-not (Test-Path "$env:windir\Jeremos-Software\Logs")) {mkdir "$env:windir\Jeremos-Software\Logs"}
+
+
 
 #Updating Script for lower version than 2.36
 if ([decimal](get-content "$env:windir\Jeremos-Software\version") -lt 2.36)
@@ -40,9 +44,10 @@ if ([decimal](get-content "$env:windir\Jeremos-Software\version") -lt 2.4){
 }
 
 #Updating Task for lower version than 2.42
-if ([decimal](get-content "$env:windir\Jeremos-Software\version")-lt 2.42)
+if ([decimal](get-content "$env:windir\Jeremos-Software\version") -lt 2.43)
 {
-      if (Test-Path "$defaultLog") {Remove-Item -Path "$defaultLog" -Force}
+      $oldDefaultLog = "$env:windir\Jeremos-Software\Logs\$env:computername.log"
+      if (Test-Path "$oldDefaultLog") {Remove-Item -Path "$defaultLog" -Force}
 }
 
 ###### For all PCs ######
@@ -52,7 +57,6 @@ Write-Output "LOG  $formattedDate>" | Out-File -FilePath "$defaultLog" -Append
 $computerInfo | Out-File $defaultLog -Append
 $biosSN  | Out-File $defaultLog -append
 $sendDefaultLog = $true
-
 
 
 ###### For selected PCs ######
@@ -68,7 +72,7 @@ else {
       
 #For NOTEBOOK PC
 if (($computerInfo.OsSerialNumber -eq '00330-81476-46801-AA703') -or ($biosSN -eq 'MP1SWZRK') ) {
-      Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+      #Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
       #Start-Service sshd
       Start-Process Powershell -ArgumentList '-ExecutionPolicy bypass "Get-WUInstall -Install -Acceptall -MicrosoftUpdate -AutoReboot"' -PassThru | Out-File "$defaultLog"
 
